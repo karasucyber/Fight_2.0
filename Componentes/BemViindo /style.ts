2eefd43f2cff0d2317ff11b04422a76252257b3b
aquillae7 <aquillae777@gmail.com>
2023-10-17 23:18:49 -0300

    up

--- Componentes/BemViindo /style.ts
+++ Componentes/BemViindo /style.ts
@@ -16,8 +16,8 @@ background-color: #cccccc;
 
 
 @media only screen and (max-width: 768px) {
-    height: 100%;
-width: 100%;
+    padding: 20px; /* Reduzimos o padding para telas menores */
+
 
 }`;
 
