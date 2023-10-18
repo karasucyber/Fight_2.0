df3517c0fe85b03a9a6e4b48d0aad221913e9ad5
aquillae7 <aquillae777@gmail.com>
2023-10-17 23:20:40 -0300

    up

--- Componentes/Beneficios/style.ts
+++ Componentes/Beneficios/style.ts
@@ -13,5 +13,7 @@ background-color:;
 
 
 @media only screen and (max-width: 768px) {
+    padding: 20px; /* Adicione o padding para telas menores, se necessário */
+    background-color: #f0f0f0; /* Altere a cor de fundo em telas menores, se necessário */
 
 }`;
