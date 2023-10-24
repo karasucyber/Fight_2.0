6b52a86c47742c5875e007cd4c7586b506751ae0
aquillae7 <aquillae777@gmail.com>
2023-10-23 23:28:17 -0300

    up

--- Componentes/Footer/Footer.tsx
+++ Componentes/Footer/Footer.tsx
@@ -2,35 +2,40 @@ import React from "react";
 import * as S from "./style";
 import { IonIcon } from '@ionic/react';
 import { Icon } from "ionicons/dist/types/components/icon/icon";
-
+import { Button } from "reactstrap";
 
 export const Footer = ()=>{
     return(<>
+    <S.ContainerBox> 
     <S.Container>
         <S.Column> 
-            <h1> Atendimento </h1>
-            <p> xt ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with th </p>
-        
-            <p> Ligue ou chame no whatsapp </p>
-            <p> (11) 999999999 </p>
-
-            <p> Nos envie um email </p>
+            <S.Titulo> Atendimento </S.Titulo>
+            <S.Paragrafo> It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like) </S.Paragrafo>
+            
+            <S.ContainerTexto> 
+                <S.Titulo3> Nós envie :</S.Titulo3>
             <p> email@jsndajsfnas.com.br </p>
-
+            </S.ContainerTexto>
+    
         </S.Column>
-        <S.Column> 
-            <h1> Home fight </h1>
-            <p> It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</p>
 
+        <S.Column> 
+            <S.Titulo2> Home fight</S.Titulo2>
+            <S.Paragrafo> It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like) </S.Paragrafo>
         </S.Column>
-        <S.Column> 
-            <h1> Nós siga nas redes socias</h1>
-            <S.ContainerIcons><IonIcon name="logo-instagram"></IonIcon> <a> Home_fight</a> <IonIcon name="logo-tiktok"></IonIcon> <a>@Homefight_</a></S.ContainerIcons>
 
+        <S.Column> 
+            <h3> Nós siga nas redes socias</h3>
+            <S.ContainerIcons><Button><IonIcon name="logo-instagram"></IonIcon><a>@Homefight_</a></Button> 
+             <Button><IonIcon name="logo-tiktok"></IonIcon> <a>@Homefight_</a></Button>
+             </S.ContainerIcons>
+
+            <S.ContainerIcons>
+                <h3> Formas de Pagamento </h3>
+            </S.ContainerIcons>
         </S.Column>
 
     </S.Container>
-    
-    
+    </S.ContainerBox>
     </>)
 }
--- Componentes/Footer/style.ts
+++ Componentes/Footer/style.ts
@@ -2,12 +2,19 @@ import React from 'react';
 import styled from 'styled-components';
 
 export const Container = styled.div`
-  display: flex;
-  justify-content: space-between;
-  background: black;
-  height: 100%;
-  width: 100%;
+display: flex;
+justify-content: space-between;
+height: 100%;
+width: 100%;
+ 
 `;
+
+export const ContainerBox = styled.div`
+background: black;  
+padding: 40px;
+`;
+
+
 
 export const Column = styled.div`
   flex: 1;
@@ -19,15 +26,27 @@ export const Column = styled.div`
 
 
 export const Titulo = styled.div`
-font-siza: 24px;
+padding: 5px;
+font-size: 24px;
 `
 
-export const Paragrafo = styled.div` 
+export const Paragrafo = styled.div`
+padding: 5px;
 font-size: 12px;
 
+`
+export const ContainerTexto = styled.div`
+margin: 2px;
 `
 
 export const ContainerIcons = styled.div`
 padding: 10px;
+`
 
+export const Titulo2 = styled.h2`
+font-size: 20px;
+`
+
+export const Titulo3 = styled.h2`
+font-size: 15px;
 `
