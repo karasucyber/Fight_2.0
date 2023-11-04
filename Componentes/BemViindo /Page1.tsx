e9965dc76a571afcd4d0c7d4023977a0a8a1725c
aquillae7 <aquillae777@gmail.com>
2023-11-04 19:07:53 -0300

    rm button and update

--- Componentes/BemViindo /Page1.tsx
+++ Componentes/BemViindo /Page1.tsx
@@ -3,6 +3,7 @@ import React from "react"
 import * as S from './style';
 import Page2 from "../Video/page2";
 import { IonIcon } from '@ionic/react';
+import { Button } from "reactstrap";
 
 
 
@@ -13,7 +14,21 @@ const Page1 = () => {
       <S.ContainerTex>
       <S.Titulo>Treine sem sair de casa </S.Titulo>
       <S.Paragrafo>levamos o treino até você</S.Paragrafo>
-       <S.Butao href="https://www.instagram.com/homefight_/"  target="_blank" >  Eu quero <IonIcon name="caret-forward-outline"></IonIcon></S.Butao>
+      <Button
+  style={{
+    backgroundColor: 'transparent',
+    color: 'white',
+    fontSize: '20px',
+    cursor: 'pointer',
+    display: 'flex',
+    alignItems: 'center',
+    textShadow: '2px 2px 4px rgba(0, 0, 0, 1)',
+  }}
+  href="https://www.instagram.com/homefight_/"
+  target="_blank"
+>
+  Eu quero <IonIcon name="caret-forward-outline" />
+</Button>
         </S.ContainerTex>
         <Page2></Page2>
     </S.Container>
