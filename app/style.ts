9938b08d3b08a5ca13faa7edf13698c072cd0079
karasucyber <aquillae777@gmil.com>
2023-11-07 23:41:31 -0300

    sem margin

--- app/style.ts
+++ app/style.ts
@@ -3,12 +3,19 @@ import styled from "styled-components";
 export const ContainerMain = styled.div`
   @import url('https://fonts.googleapis.com/css2?family=EB+Garamond&display=swap');
 
-  height: 100%;
+  height: 110%;
   margin: 0;
   padding: 0;
+  border: 0;
   font-family: 'EB Garamond', serif; /* Use o nome da fonte importada */
   background: url('caminho/para/sua/imagem.jpg') no-repeat center center fixed; /* Substitua o caminho da imagem */
   background-size: cover;
+  display: flex;
+flex-direction: column;
+justify-content: center;
+align-items: center;
+height: 100%;
+width: 100%;
 
   `;
   
